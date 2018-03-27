983f632c371fcba458b6487c9ed3748c5b25a2a9
Stephen Phillips <sbphillips19@gmail.com>
2018-03-26 18:59:16 -0800

    redux project

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Book 1', pages: 10},
+    { title: 'Book 2', pages: 20},
+    { title: 'Book 3', pages: 30},
+    { title: 'Book 4', pages: 40}
+  ];
+}
